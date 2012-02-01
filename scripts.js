e3a4d5bfeab2384e8c62a4d8ee4ef4668fe6447c
Alykhan Kanji <a.kanji@me.com>
2012-02-01 00:35:52 -0500

    Add scripts.js

---
+++ scripts.js
@@ -0,0 +1,47 @@
+// Author: ricocheting.com
+// For: public release (freeware)
+// Date: 4/24/2003 (update: 6/26/2009)
+// Description: displays the amount of time until the "dateFuture" entered below.
+// NOTE: the month entered must be one less than current month. ie; 0=January, 11=December
+// NOTE: the hour is in 24 hour format. 0=12am, 15=3pm etc
+// format: dateFuture = new Date(year,month-1,day,hour,min,sec)
+// example: dateFuture = new Date(2003,03,26,14,15,00) = April 26, 2003 - 2:15:00 pm
+
+function GetCount(){
+
+	dateFuture = new Date(2011,1,7,17,30,00);
+	dateNow = new Date();								//grab current date
+	amount = dateFuture.getTime() - dateNow.getTime();		//calc milliseconds between dates
+	delete dateNow;
+
+	// time is already past
+	if(amount < 0){
+		document.getElementById('countbox').innerHTML="The conference has commenced with the keynote address by Dr. Izzeldin Abuelaish at 5:30 PM on Monday February 7<sup>th</sup>, 2011.";
+	}
+	// date is still good
+	else{
+		days=0;hours=0;mins=0;secs=0;out="";
+
+		amount = Math.floor(amount/1000);//kill the "milliseconds" so just secs
+
+		days=Math.floor(amount/86400);//days
+		amount=amount%86400;
+
+		hours=Math.floor(amount/3600);//hours
+		amount=amount%3600;
+
+		mins=Math.floor(amount/60);//minutes
+		amount=amount%60;
+
+		secs=Math.floor(amount);//seconds
+
+		if(days != 0){out += days +" day"+((days!=1)?"s":"")+", ";}
+		if(days != 0 || hours != 0){out += hours +" hour"+((hours!=1)?"s":"")+", ";}
+		if(days != 0 || hours != 0 || mins != 0){out += mins +" minute"+((mins!=1)?"s":"")+", ";}
+		out += secs +" seconds";
+		out = out + " until the conference begins with the keynote address by Dr. Izzeldin Abuelaish at 5:30 PM on Monday February 7<sup>th</sup>, 2011.";
+		document.getElementById('countbox').innerHTML=out;
+
+		setTimeout("GetCount()", 500);
+	}
+}
